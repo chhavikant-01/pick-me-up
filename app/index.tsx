c512bfc668eeb9e1fe9a1786b880c83bbdd45182
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-12-31 18:26:08 +0530

    sign-in & sign-up done

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Redirect } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Home = () => {
-  return <Redirect href="(auth)/welcome" />;
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
